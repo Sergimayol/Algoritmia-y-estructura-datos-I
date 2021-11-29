6b1c01509853bc6bd03dbf13dcde438cae914985
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2021-11-29 18:12:18 +0100

    Add files via upload

--- ListaNodos.java
+++ ListaNodos.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
 /**
  *
  * @author Sergi
@@ -9,6 +16,24 @@ public class ListaNodos {
 
     //Constructor
     public ListaNodos() {
+        primero = null;
+        libres = null;
+    }
+
+    /**
+     * Consulta si la lista esta vacia.
+     *
+     * @return true si el primer nodo (inicio), no apunta a otro nodo.
+     */
+    public boolean esVacia() {
+        return primero == null;
+    }
+
+    /**
+     * Elimina la lista
+     */
+    public void eliminar() {
+        // Elimina el valor y la referencia a los demas nodos.
         primero = null;
     }
 
@@ -75,8 +100,29 @@ public class ListaNodos {
         primero = listAux.getPrimero();
     }
 
+    public void insertarNodoFinal(int valor) {
+        // Define un nuevo nodo.
+        Nodo nuevo = new Nodo(valor);
+        // Consulta si la lista esta vacia.
+        if (esVacia()) {
+            // Inicializa la lista agregando como primero al nuevo nodo.
+            primero = nuevo;
+            // Caso contrario recorre la lista hasta llegar al ultimo nodo
+            //y agrega el nuevo.
+        } else {
+            // Crea ua copia de la lista.
+            Nodo aux = primero;
+            // Recorre la lista hasta llegar al ultimo nodo.
+            while (aux.getSiguienteNodo() != null) {
+                aux = aux.getSiguienteNodo();
+            }
+            // Agrega el nuevo nodo al final de la lista.
+            aux.setSiguiente(nuevo);
+        }
+    }
+
     //Inserta un nodo en la lista en la primera posición
-    public void insertarNodoIzq(int dato1, int dato2) {
+    public void insertarNodoInicio(int dato1, int dato2) {
         boolean existe = false;
         Nodo aux, nuevoNodo = new Nodo(dato1);
         aux = primero;
@@ -105,6 +151,81 @@ public class ListaNodos {
         }
     }
 
+    /**
+     * Inserta un nuevo nodo despues de otro, ubicado por el valor que contiene.
+     *
+     * @param referencia valor del nodo anterios al nuevo nodo a insertar.
+     * @param valor del nodo a insertar.
+     */
+    public void insertarPorReferencia(int referencia, int valor) {
+        // Define un nuevo nodo.
+        Nodo nuevo = new Nodo(valor);
+        // Verifica si la lista contiene elementos
+        if (!esVacia()) {
+            // Consulta si el valor existe en la lista.
+            if (buscar(referencia)) {
+                // Crea ua copia de la lista.
+                Nodo aux = primero;
+                // Recorre la lista hasta llegar al nodo de referencia.
+                while (aux.getInfo() != referencia) {
+                    aux = aux.getSiguienteNodo();
+                }
+                // Crea un respaldo de la continuación de la lista.
+                Nodo siguiente = aux.getSiguienteNodo();
+                // Enlaza el nuevo nodo despues del nodo de referencia.
+                aux.setSiguiente(nuevo);
+                // Une la continuacion de la lista al nuevo nodo.
+                nuevo.setSiguiente(siguiente);
+            }
+        }
+    }
+
+    /**
+     * Inserta un nuevo nodo despues en una posición determinada.
+     *
+     * @param posicion en la cual se va a insertar el nuevo nodo.
+     * @param valor valor del nuevo nodo de la lista.
+     */
+    public void insrtarPorPosicion(int posicion, int valor) {
+        // Verifica si la posición ingresada se encuentre en el rango
+        // >= 0 y <= que el numero de elementos del la lista.
+        if (posicion >= 0 && posicion <= getLongitudLista()) {
+            Nodo nuevo = new Nodo(valor);
+            // Consulta si el nuevo nodo a ingresar va al primero de la lista.
+            if (posicion == 0) {
+                // Inserta el nuevo nodo al primero de la lista.
+                nuevo.setSiguiente(primero);
+                primero = nuevo;
+            } else {
+                // Si el nodo a inserta va al final de la lista.
+                if (posicion == getLongitudLista()) {
+                    Nodo aux = primero;
+                    // Recorre la lista hasta llegar al ultimo nodo.
+                    while (aux.getSiguienteNodo() != null) {
+                        aux = aux.getSiguienteNodo();
+                    }
+                    // Inserta el nuevo nodo despues de del ultimo.
+                    aux.setSiguiente(nuevo);
+                } else {
+                    // Si el nodo a insertar va en el medio de la lista.
+                    Nodo aux = primero;
+                    // Recorre la lista hasta llegar al nodo anterior
+                    // a la posicion en la cual se insertara el nuevo nodo.
+                    for (int i = 0; i < (posicion - 1); i++) {
+                        aux = aux.getSiguienteNodo();
+                    }
+                    // Guarda el nodo siguiente al nodo en la posición
+                    // en la cual va a insertar el nevo nodo.
+                    Nodo siguiente = aux.getSiguienteNodo();
+                    // Inserta el nuevo nodo en la posición indicada.
+                    aux.setSiguiente(nuevo);
+                    // Une el nuevo nodo con el resto de la lista.
+                    nuevo.setSiguiente(siguiente);
+                }
+            }
+        }
+    }
+
     //Imprime todos los campos info de los nodos
     public void getLista() {
         Nodo aux;
@@ -129,9 +250,10 @@ public class ListaNodos {
         return "Lista{" + "primero = " + primero + '}';
     }
 
-//    public String libres(){
-//        return "Lista basura = " + libres;
-//    }
+    public String libres() {
+        return "Lista basura = " + libres;
+    }
+
     //Devuelve la longitud de la lista
     public int getLongitudLista() {
         Nodo aux;
@@ -155,6 +277,204 @@ public class ListaNodos {
             aux = aux.getSiguienteNodo();
         }
         return aux3;
+    }
+
+    /**
+     * Obtiene el valor de un nodo en una determinada posición.
+     *
+     * @param posicion del nodo que se desea obtener su valor.
+     * @return un numero entero entre [0,n-1] n = numero de nodos de la lista.
+     * @throws Exception
+     */
+    public int getValor(int posicion) throws Exception {
+        // Verifica si la posición ingresada se encuentre en el rango
+        // >= 0 y < que el numero de elementos del la lista.
+        if (posicion >= 0 && posicion < getLongitudLista()) {
+            // Consulta si la posicion es el primero de la lista.
+            if (posicion == 0) {
+                // Retorna el valor del primero de la lista.
+                return primero.getInfo();
+            } else {
+                // Crea una copia de la lista.
+                Nodo aux = primero;
+                // Recorre la lista hasta la posición ingresada.
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getSiguienteNodo();
+                }
+                // Retorna el valor del nodo.
+                return aux.getInfo();
+            }
+            // Crea una excepción de Posicion inexistente en la lista.
+        } else {
+            throw new Exception("Posicion inexistente en la lista.");
+        }
+    }
+
+    /**
+     * Busca si existe un valor en la lista.
+     *
+     * @param referencia valor a buscar.
+     * @return true si existe el valor en la lista.
+     */
+    public boolean buscar(int referencia) {
+        // Crea una copia de la lista.
+        Nodo aux = primero;
+        // Bandera para indicar si el valor existe.
+        boolean encontrado = false;
+        // Recorre la lista hasta encontrar el elemento o hasta 
+        // llegar al final de la lista.
+        while (aux != null && encontrado != true) {
+            // Consulta si el valor del nodo es igual al de referencia.
+            if (referencia == aux.getInfo()) {
+                // Canbia el valor de la bandera.
+                encontrado = true;
+            } else {
+                // Avansa al siguiente. nodo.
+                aux = aux.getSiguienteNodo();
+            }
+        }
+        // Retorna el resultado de la bandera.
+        return encontrado;
+    }
+
+    /**
+     * Consulta la posición de un elemento en la lista
+     *
+     * @param referencia valor del nodo el cual se desea saber la posición.
+     * @return un valor entero entre [0,n] que indica la posición del nodo.
+     * @throws Exception
+     */
+    public int getPosicion(int referencia) throws Exception {
+        // Consulta si el valor existe en la lista.
+        if (buscar(referencia)) {
+            // Crea una copia de la lista.
+            Nodo aux = primero;
+            // COntado para almacenar la posición del nodo.
+            int cont = 0;
+            // Recoore la lista hasta llegar al nodo de referencia.
+            while (referencia != aux.getInfo()) {
+                // Incrementa el contador.
+                cont++;
+                // Avansa al siguiente. nodo.
+                aux = aux.getSiguienteNodo();
+            }
+            // Retorna el valor del contador.
+            return cont;
+            // Crea una excepción de Valor inexistente en la lista.
+        } else {
+            throw new Exception("Valor inexistente en la lista.");
+        }
+    }
+
+    /**
+     * Actualiza el valor de un nodo que se encuentre en la lista ubicado por un
+     * valor de referencia.
+     *
+     * @param referencia valor del nodo el cual se desea actualizar.
+     * @param valor nuevo valor para el nodo.
+     */
+    public void editarPorReferencia(int referencia, int valor) {
+        // Consulta si el valor existe en la lista.
+        if (buscar(referencia)) {
+            // Crea ua copia de la lista.
+            Nodo aux = primero;
+            // Recorre la lista hasta llegar al nodo de referencia.
+            while (aux.getInfo() != referencia) {
+                aux = aux.getSiguienteNodo();
+            }
+            // Actualizamos el valor del nodo
+            aux.setInfo(valor);
+        }
+    }
+
+    /**
+     * Actualiza el valor de un nodo que se encuentre en la lista ubicado por su
+     * posición.
+     *
+     * @param posicion en la cual se encuentra el nodo a actualizar.
+     * @param valor nuevo valor para el nodo.
+     */
+    public void editarPorPosicion(int posicion, int valor) {
+        // Verifica si la posición ingresada se encuentre en el rango
+        // >= 0 y < que el numero de elementos del la lista.
+        if (posicion >= 0 && posicion < getLongitudLista()) {
+            // Consulta si el nodo a eliminar es el primero.
+            if (posicion == 0) {
+                // Alctualiza el valor delprimer nodo.
+                primero.setInfo(valor);
+            } else {
+                // En caso que el nodo a eliminar este por el medio 
+                // o sea el ultimo
+                Nodo aux = primero;
+                // Recorre la lista hasta lleger al nodo anterior al eliminar.
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getSiguienteNodo();
+                }
+                // Alctualiza el valor del nodo.
+                aux.setInfo(valor);
+            }
+        }
+    }
+
+    /**
+     * Elimina un nodo que se encuentre en la lista ubicado por un valor de
+     * referencia.
+     *
+     * @param referencia valor del nodo que se desea eliminar.
+     */
+    public void removerPorReferencia(int referencia) {
+        // Consulta si el valor de referencia existe en la lista.
+        if (buscar(referencia)) {
+            // Consulta si el nodo a eliminar es el pirmero
+            if (primero.getInfo() == referencia) {
+                // El primer nodo apunta al siguiente.
+                primero = primero.getSiguienteNodo();
+            } else {
+                // Crea ua copia de la lista.
+                Nodo aux = primero;
+                // Recorre la lista hasta llegar al nodo anterior 
+                // al de referencia.
+                while (aux.getSiguienteNodo().getInfo() != referencia) {
+                    aux = aux.getSiguienteNodo();
+                }
+                // Guarda el nodo siguiente del nodo a eliminar.
+                Nodo siguiente = aux.getSiguienteNodo().getSiguienteNodo();
+                // Enlaza el nodo anterior al de eliminar con el 
+                // sguiente despues de el.
+                aux.setSiguiente(siguiente);
+            }
+        }
+    }
+
+    /**
+     * Elimina un nodo que se encuentre en la lista ubicado por su posición.
+     *
+     * @param posicion en la cual se encuentra el nodo a eliminar.
+     */
+    public void removerPorPosicion(int posicion) {
+        // Verifica si la posición ingresada se encuentre en el rango
+        // >= 0 y < que el numero de elementos del la lista.
+        if (posicion >= 0 && posicion < getLongitudLista()) {
+            // Consulta si el nodo a eliminar es el primero
+            if (posicion == 0) {
+                // Elimina el primer nodo apuntando al siguinte.
+                primero = primero.getSiguienteNodo();
+            } // En caso que el nodo a eliminar este por el medio 
+            // o sea el ultimo
+            else {
+                // Crea una copia de la lista.
+                Nodo aux = primero;
+                // Recorre la lista hasta lleger al nodo anterior al eliminar.
+                for (int i = 0; i < posicion - 1; i++) {
+                    aux = aux.getSiguienteNodo();
+                }
+                // Guarda el nodo siguiente al nodo a eliminar.
+                Nodo siguiente = aux.getSiguienteNodo();
+                // Elimina la referencia del nodo apuntando al nodo siguiente.
+                aux.setSiguiente(siguiente.getSiguienteNodo());
+            }
+
+        }
     }
 
     private class Nodo {
